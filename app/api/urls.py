02049db48427db8c39d09e35795524a9586cf5f0
Bala Viswanadh <balaviswanadh21@gmail.com>
2022-01-11 22:44:07 +0530

    API for Blogs List and Detail

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app.api.views import AuthorsListView, AuthorBlogsView
+from app.api.views import AuthorsListView, AuthorBlogsView, BlogsListView, BlogDetailView
 
 urlpatterns = [
     path('authors/',AuthorsListView.as_view()),
     path('author/<int:pk>/blogs',AuthorBlogsView.as_view()), 
+    path('blogs/',BlogsListView.as_view()),
+    path('blog/<int:pk>/',BlogDetailView.as_view()),
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,17 +1,17 @@
 from rest_framework.exceptions import ValidationError
 from app.models import Author, Blog, Comment
 from app.api.serializers import AuthorSerializer, BlogSerializer
-from rest_framework import mixins,generics
+from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
 #API for Listing Authors
-class AuthorsListView(generics.ListAPIView):
+class AuthorsListView(ListAPIView):
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
     
 #API for Listing Blogs of Author
-class AuthorBlogsView(generics.ListAPIView):
+class AuthorBlogsView(ListAPIView):
     serializer_class = BlogSerializer
     
     def get_queryset(self):
@@ -22,4 +22,22 @@ class AuthorBlogsView(generics.ListAPIView):
             raise ValidationError({'Invalid Request': 'Author doesnot exist'})
         return Blog.objects.filter(author=author)
     
+#API for Listing All blogs
+class BlogsListView(ListAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+    
+#API for Details of Specific Blog
+class BlogDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+    
+    # def get_queryset(self):
+    #     pk = self.kwargs['pk']
+    #     try:
+    #         blog = Blog.objects.get(pk=pk)
+    #         print('blog is ',blog)
+    #     except Blog.DoesNotExist:
+    #         raise ValidationError({'Invalid Request': 'Blog doesnot exist'})
+    #     return Blog.objects.get(pk=pk)
             
